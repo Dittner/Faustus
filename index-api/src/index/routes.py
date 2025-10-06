48b5a8974c93a706c8bb1ab18be2fe1fcd6e86a4
Alexander Dittner <alexander.dittner@gmail.com>
2025-10-06 23:11:43 +0300

    Pathlib instead os.path

--- index-api/src/index/routes.py
+++ index-api/src/index/routes.py
@@ -1,110 +1,106 @@
+from pathlib import Path
+
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
-import os
 
 from src.index.schemas import TextFileSchema
 
-ROOT_DIR = 'data'
+ROOT_DIR_PATH = Path('data')
+TRASH_DIR_PATH = Path('data/trash')
+
+if not ROOT_DIR_PATH.exists():
+    Path.mkdir(ROOT_DIR_PATH)
+
+if not TRASH_DIR_PATH.exists():
+    Path.mkdir(TRASH_DIR_PATH)
 
 router = APIRouter()
 
 
 @router.get('/')
 def index_root():
-    return {"result": "index api is ready!"}
+    return {'result': 'index api is ready!'}
 
 
 # ---------------
 #     dir
 # ---------------
-def isValidTextFile(filePath):
-    fileTitle, fileExtension = split(filePath, '.')
-    return os.path.isfile(filePath) and not fileTitle.startswith('.') and fileExtension == 'txt'
+def is_valid_text_file(p: Path):
+    return p.exists() and p.is_file() and not p.stem.startswith('.') and p.suffix == '.txt'
 
 
-def split(txt, rdelim):
-    ind = txt.find(rdelim)
-    if ind == -1:
-        return (txt, '')
-    return (txt[0:ind], txt[ind + 1:])
+def mkdirs(path: str):
+    dir_names = path.split('/')
+    ind = 1 if len(dir_names) > 0 and dir_names[0] == ROOT_DIR_PATH.as_posix() else 0
+    p = ROOT_DIR_PATH
+    while ind < len(dir_names):
+        n = dir_names[ind]
+        p = p / n
+        if n.endswith('.txt'):
+            break
 
-
-def mkdirs(path):
-    dirNames = path.split('/')
-    ind = 1 if len(dirNames) > 0 and dirNames[0] == ROOT_DIR else 0
-    path = ROOT_DIR
-    while ind < len(dirNames):
-        n = dirNames[ind]
-        path += '/' + n
-        if n.endswith('.txt'): break
-        if not os.path.isdir(path):
+        if not p.exists():
             print('Creating new dir:', path)
-            os.mkdir(path)
+            Path.mkdir(p)
         ind += 1
-    pass
 
 
 @router.get('/dir')
 @router.get('/dir/{src:path}')
 def list_dir(src: str = ''):
-    if not os.path.isdir(ROOT_DIR):
-        os.mkdir(ROOT_DIR)
-        return 'Dir not found: ' + src, 404
-
-    path = ROOT_DIR + '/' + src
-    if not os.path.isdir(path):
+    p = ROOT_DIR_PATH / src
+    if not p.is_dir():
         return 'Not a dir: ' + src, 404
 
     files = []
-    names = os.listdir(path)
 
-    print('API::list_dir, src:', src, 'names:', names)
+    # print('API::list_dir, src:', p.as_posix(), ', children:', list(p.iterdir()))
 
-    for name in names:
-        if name.startswith('.') or name == 'info.txt': continue
-        itemPath = path + '/' + name
-        if os.path.isdir(itemPath):
-            infoFilePath = path + '/' + name + '/info.txt'
-            if isValidTextFile(infoFilePath):
-                infoFile = open(infoFilePath, 'rt')
-                info = infoFile.read()
-                infoFile.close()
-                files.append({'isDirectory': True, 'path': itemPath, 'text': info})
-        elif isValidTextFile(itemPath):
-            # fileTitle, fileExtension = split(name, '.')
-            f = open(itemPath, 'rt')
-            fileContent = f.read()
+    for child in p.iterdir():
+        if child.name.startswith('.') or child.name == 'info.txt':
+            continue
+
+        if child.is_dir():
+            info_file_path: Path = child / 'info.txt'
+            if is_valid_text_file(info_file_path):
+                info_file = info_file_path.open('rt')
+                info = info_file.read()
+                info_file.close()
+                files.append({'isDirectory': True, 'path': child.as_posix(), 'text': info})
+        elif is_valid_text_file(child):
+            f = child.open('rt')
+            file_content = f.read()
             f.close()
-            files.append({'isDirectory': False, 'path': itemPath, 'text': fileContent})
+            files.append({'isDirectory': False, 'path': child.as_posix(), 'text': file_content})
 
     print('API::list_dir, files count:', len(files))
     return files
 
 
-
-#---------------
+# ---------------
 #     file
-#---------------
+# ---------------
+
 
 @router.get('/file/{src:path}')
-def read_file(src):
+def read_file(src: str):
     print('API::read_file, filePath:', src)
-    path = ROOT_DIR + '/' + src
+    p = ROOT_DIR_PATH / src
 
-    if not os.path.isfile(path):
+    if not p.exists():
         return 'File not found: ' + src, 404
 
-    file = open(path, 'rt')
-    fileContent = file.read()
+    file = p.open('rt')
+    file_content = file.read()
     file.close()
-    return fileContent
+    return file_content
 
 
 @router.post('/file/{src:path}')
-def write_file(tf:TextFileSchema, src):
-    path = ROOT_DIR + '/' + src
-    if not src.endswith('.txt'):
-        print('Not a file:', path)
+def write_file(tf: TextFileSchema, src: str):
+    p = ROOT_DIR_PATH / src
+    if p.suffix != '.txt':
+        print('Not a file:', p.as_posix())
         return 'Not a file: ' + src, 400
 
     mkdirs(src)
@@ -112,65 +108,66 @@ def write_file(tf:TextFileSchema, src):
     names = src.split('/')
     count = len(names)
 
-    oldPath = ''
-    newPath = ''
+    old_path = ROOT_DIR_PATH / src
+    new_path = ROOT_DIR_PATH / src
 
-    if count > 1 and names[count-1] == 'info.txt':
-        fileName = names[count-2]
-        if fileName != tf.id:
-            names[count-1] = ''
-            oldPath = ROOT_DIR + '/' + '/'.join(names)
-            names[count-2] = tf.id
-            newPath = ROOT_DIR + '/' + '/'.join(names)
-            print('New dir path:', newPath)
-            if os.path.isdir(newPath):
-                print('Dir already exists:', newPath)
-                return 'Dir already exists: ' + newPath, 400
+    # directory case
+    if p.name == 'info.txt':
+        dir_name = names[count - 2]
+        if dir_name != tf.id:
+            names[count - 1] = ''
+            names[count - 2] = tf.id
+            new_path = ROOT_DIR_PATH / '/'.join(names)
+            print('New dir path:', new_path)
+            if Path.is_dir(Path(new_path)):
+                print('Dir already exists:', new_path)
+                return 'Dir already exists: ' + new_path, 400
+    # file case
     elif count > 0:
-        fileName, fileExtension = split(names[count-1], '.')
-        if fileName != tf.id:
-            names[count-1] = tf.id + '.txt'
-            oldPath = path
-            newPath = ROOT_DIR + '/' + '/'.join(names)
-            print('New file path:', newPath)
-            if os.path.isfile(newPath):
-                print('File already exists:', newPath)
-                return 'File already exists: ' + newPath, 400
+        file_name = p.stem
+        if file_name != tf.id:
+            names[count - 1] = tf.id + '.txt'
+            new_path = ROOT_DIR_PATH / '/'.join(names)
+            print('New file path:', new_path)
+            if new_path.exists():
+                print('File already exists:', new_path)
+                return 'File already exists: ' + new_path, 400
 
-    f = open(path, 'wt')
+    f = p.open('wt')
     f.write(tf.text)
     f.close()
 
-    if len(oldPath) > 0 and len(newPath) > 0:
-        print('Renaming from:', oldPath, ', to:', newPath)
-        os.rename(oldPath, newPath)
+    if new_path != old_path:
+        print('Renaming from:', old_path.as_posix(), 'to:', new_path.as_posix())
+        old_path.rename(new_path)
+
     return 'ok', 200
 
 
 @router.delete('/file/{src:path}')
-def delete_file(src):
-    path = ROOT_DIR + '/' + src
-    print('Deleting file, path:', path)
-    if not os.path.isfile(path) and not os.path.isdir(path):
+def delete_file(src: str):
+    p = ROOT_DIR_PATH / src
+    print('Deleting file, path:', p.as_posix())
+    if not p.exists():
         return 'File not found: ' + src, 404
 
-    newPath = path.replace('/index/', '/bin/', 1)
+    new_path = Path('trash/' + p.name)
 
-    if newPath != path:
-        mkdirs(newPath)
-        os.replace(path, newPath)
-        print('Deleting file has been moved to:', newPath)
+    if new_path != p:
+        p.replace(new_path)
+        print('Deleting file has been moved to:', new_path.as_posix())
 
     return 'ok', 200
 
-#---------------
+
+# ---------------
 #     ASSETS
-#---------------
+# ---------------
 @router.get('/asset/{src:path}')
-def get_image(src):
-    path = ROOT_DIR + '/' + src
-    print('get_image::path:', path)
-    if not os.path.isfile(path):
+def get_image(src: str):
+    p = ROOT_DIR_PATH / src
+    print('get_image::path:', p.as_posix())
+    if not p.exists():
         return {'result': 'Asset not found, path: ' + src}
 
-    return FileResponse(path)
+    return FileResponse(p.as_posix())
--- index-api/src/main.py
+++ index-api/src/main.py
@@ -3,19 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.index.routes import router as index_router
 
-
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
-)
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
 
-app.include_router(index_router, prefix="/api/index")
+app.include_router(index_router, prefix='/api/index')
 
 
-@app.get("/api", summary="root", tags=["Root rout"])
+@app.get('/api', summary='root', tags=['Root rout'])
 async def root():
-    return {"status": "ready"}
+    return {'status': 'ready'}
 
 
 # if __name__ == "__main__":
